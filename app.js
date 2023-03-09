c91a6388802e2d6c76cd0c6d6bae39a4c8f390e4
*Ajayi <*emmanueltemitayo056@gmail.com*>
2023-03-09 10:30:45 +0100

    initialized project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connectToDb } = require("./db");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// connectToDb();
+
+app.use(express.urlencoded({extended: true}))
+
+//routes
+// const shortRoute = require("./routes/short")
+
+app.use(express.json())
+app.set('view engine', 'ejs')
+// app.use('/', shortRoute)
+
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
