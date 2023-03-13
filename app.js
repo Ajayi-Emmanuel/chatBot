f420da5aa8f4beff0f221e37914e8598bf914937
*Ajayi <*emmanueltemitayo056@gmail.com*>
2023-03-13 15:40:14 +0100

    Completed the front-end chat ui

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const express = require("express");
-const { connectToDb } = require("./db");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-// connectToDb();
+
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
-// const shortRoute = require("./routes/short")
+const botRoute = require("./routes/bot") 
 
 app.use(express.json())
 app.set('view engine', 'ejs')
-// app.use('/', shortRoute)
+app.use('/bot', botRoute)
 
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
+});
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connected`);
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
 });
---
+++ public/main.js
@@ -0,0 +1,6 @@
+// const socket = io();
+
+// const messageForm = document.getElementById("send-container");
+// const messageInput = document.getElementById("message-input");
+// const nameInput = document.getElementById("name-input");
+// const messageForm = document.getElementById("send-container");
