4a29331f1d78ef8d713090ed2ce27ebd22d878b7
*Ajayi <*emmanueltemitayo056@gmail.com*>
2023-03-14 05:43:18 +0100

    changed html page to ejs and passed input from front end framework to backend

--- public/main.js
+++
@@ -1,6 +0,0 @@
-// const socket = io();
-
-// const messageForm = document.getElementById("send-container");
-// const messageInput = document.getElementById("message-input");
-// const nameInput = document.getElementById("name-input");
-// const messageForm = document.getElementById("send-container");
--- routes/bot.js
+++ routes/bot.js
@@ -1,2 +1,12 @@
+const botroute = require("express").Router();
+
+botroute.get('/', (req, res) => {
+    res.render("index.ejs")
+})
+botroute.post('/message', (req, res) => {
+    const {message} = req.body;
+    console.log(message)
+})
 
 
+module.exports = botroute 
